3606a642ece73d6f6737cfde751b49b86f2bbe69
Gaston <77273777+gdescalzo@users.noreply.github.com>
2022-06-14 10:52:32 -0300

    Delete PS directory

--- PS/Installing a Domain Controller.ps1
+++
@@ -1,4 +0,0 @@
-﻿Install-ADDSDomainController -SkipPreChecks -DomainName gsventerprise.com.ar `
- -SiteName GSV-Tribunales -DatabasePath c:\windows\ntds `
- -LogPath c:\windows\ntds -SysvolPath c:windows\sysvol -InstallDns `
- -NoRebootOnCompletion:$false -Force:$true
