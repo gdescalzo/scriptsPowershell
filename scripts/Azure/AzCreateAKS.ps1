b0e1b172f76b4c97cc8df2d9a1add135fcc1b497
Gaston <gdescalzo@gmail.com>
2023-01-29 15:44:41 -0300

    Add Azure Create AKS Cluster

--- scripts/Azure/AzCreateAKS.ps1
+++ scripts/Azure/AzCreateAKS.ps1
@@ -1,19 +1,39 @@
+<# Azure SPN (GSV-Enterprise)
+
+SPN Name			: SPN-GSV-Enterprise
+SPN Secret Name		: SPN-GSV-Enterprise-ClientSecret
+SPN Secret Value    : UIc8Q~IsxLBanGJcKdKhYMGa2xG7zByu1vsR5b9B	
+SPN Secret ID		: ea9511ad-6ab9-4726-bf6d-340b588b442e
+SPN Application ID	: e1ee79fc-976d-4d29-97e6-e935abf7d333
+Subscription Id     : 794c4aed-01e7-4a3f-b64d-4d4b02022224
+
+#>
+
 <## VARS ##>
 $Secret = "UIc8Q~IsxLBanGJcKdKhYMGa2xG7zByu1vsR5b9B"
 $ApplicationId = "e1ee79fc-976d-4d29-97e6-e935abf7d333"
-$TenantId = "2456b851-46fd-4293-907c-d1d81c679af6"
-$SubscriptionId = "794c4aed-01e7-4a3f-b64d-4d4b02022224"
+
+<## To Modify ##>
 $myResourceGroup = "GSV-RG"
-$azContext = Set-AzContext -SubscriptionId $SubscriptionId
+$myAKSCluster = "GSV-AKScluster"
+$NodeCount = "1"
+$WorkSpaceID = "GSV-WorkSpace"
 
-function AzConection {
+$SecuredPassword = ConvertTo-SecureString $Secret -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecuredPassword
 
-    $SecuredPassword = ConvertTo-SecureString $Secret -AsPlainText -Force
-    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecuredPassword
-    Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential
-   
+function AzAKScreate {
+    Get-AzAksCluster -ResourceGroupName $myResourceGroup -Name $myAKSCluster -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+    if ($notPresent) {     
+        $SecuredPassword = ConvertTo-SecureString $Secret -AsPlainText -Force
+        $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecuredPassword
+
+        New-AzAksCluster -ResourceGroupName $myResourceGroup -Name $myAKSCluster -NodeCount $NodeCount -WorkspaceResourceId $WorkSpaceID -Verbose -ServicePrincipalIdAndSecret $Credential -Debug
+    }
+    else {        
+        Write-Output "AKS Cluster exist."
+    }
 }
 
-AzConection 
-
-$azContext
+AzAKScreate
--- scripts/Azure/AzLoginSPN.ps1
+++ scripts/Azure/AzLoginSPN.ps1
@@ -9,7 +9,7 @@ Subscription Id     : 794c4aed-01e7-4a3f-b64d-4d4b02022224
 
 #>
 
-## VARS ##
+<## VARS ##>
 $Secret = "UIc8Q~IsxLBanGJcKdKhYMGa2xG7zByu1vsR5b9B"
 $ApplicationId = "e1ee79fc-976d-4d29-97e6-e935abf7d333"
 $TenantId = "2456b851-46fd-4293-907c-d1d81c679af6"
