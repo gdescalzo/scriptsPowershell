7240910704676ff68cadb073266df0ff08d7c86d
Gaston <gdescalzo@gmail.com>
2023-08-13 09:14:43 -0300

    Minor change

--- Others/dolarBlue.ps1
+++ Others/dolarBlue.ps1
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 ﻿$response = Invoke-RestMethod 'https://www.dolarsi.com/api/api.php?type=valoresprincipales' -Method 'GET' 
-=======
-$response = Invoke-RestMethod 'https://www.dolarsi.com/api/api.php?type=valoresprincipales' -Method 'GET' -Headers $headers
->>>>>>> cdcc365ea93c86f9dafc7b39d5b872a1192839a3
 
 foreach ($r in $response) {
     
@@ -13,8 +9,4 @@ foreach ($r in $response) {
 
         Write-Host $nombre"`nCompra: "$compra"`nVenta: "$venta
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cdcc365ea93c86f9dafc7b39d5b872a1192839a3
