ae375e98f26d829b18f3935955ae6d52fc04e11f
Gaston Descalzo <77273777+gdescalzo@users.noreply.github.com>
2025-07-26 14:56:02 -0300

    Update azWiki-CreatePage-1-Overview.ps1

--- AzureDevOps/Azure DevOps Wiki/scripts/azWiki-CreatePage-1-Overview.ps1
+++ AzureDevOps/Azure DevOps Wiki/scripts/azWiki-CreatePage-1-Overview.ps1
@@ -4,33 +4,21 @@ $Content = '
 
 #Application Overview
 
-## Why is the Application Require?
+## Why is the Application Required?
 example
 
-EY FUSE is a platform that enables third party financial service providers to securely access consumer banking, transaction, and other financial data in line with the ACCCs(Australian Competition and Consumer Commission) requirements by supporting their consumers in managing consent and to meet CDR(Consumer data right) compliance requirements.
+Some application overview. Who is the customer?
 
-## What the application does?
+## What does the application do?
 
 example
 
-Provides the following features:
-
-- It provides a customized user interface for each client according to the bank standards.
-
-- The data recipient bank can request for a consent to desired data holder bank from which data is to be extracted.
-
-- Here the application provides mainly the consent dashboards to view the details on the provided consents.
-
-## Who are the users of the application
-
-e.g
-- Users of this application are both data holder and data recipients bank who are accredited by ACCC, OAIC standards. 
+Describe the user journey
 
 ## How the users interact with the application
 
 e.g
-- They interact with the application through EY Fuse User Interfaces which is customized to the client bank.
-
+- They interact with the application through EY Fuse User Interfaces, which are customized to the client bank.
 
 '
 
