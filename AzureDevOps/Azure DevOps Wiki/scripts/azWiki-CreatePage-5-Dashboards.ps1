d90d6537cc20dbf6fd12bb04f8fa553e3151adb7
Gaston Descalzo <77273777+gdescalzo@users.noreply.github.com>
2025-07-26 14:59:47 -0300

    Update azWiki-CreatePage-5-Dashboards.ps1

--- AzureDevOps/Azure DevOps Wiki/scripts/azWiki-CreatePage-5-Dashboards.ps1
+++ AzureDevOps/Azure DevOps Wiki/scripts/azWiki-CreatePage-5-Dashboards.ps1
@@ -2,31 +2,31 @@ $Content = '
 
 [[_TOC_]]
 
-## List down all the Dashbaoed link here.
+## List down all the Dashbaoed links here.
 
 ## **N1 SLO**
 
-https://bigeye.sbp.eyclienthub.com/d/axCu7UWGk/n1-slo?orgId=110
+https://some_monitoring_platform.com/d/axCu7UWGk/n1-slo?orgId=110
 
 ## **N2 Performance**
 
-https://bigeye.sbp.eyclienthub.com/d/WHHuYhznz/n2-performance?orgId=110
+https://some_monitoring_platform.com/d/WHHuYhznz/n2-performance?orgId=110
 
 ## **N3 Infrastructure**
 - **API Management**  
-https://bigeye.sbp.eyclienthub.com/d/-rfNCFznz/n3-api-management?orgId=110
+https://some_monitoring_platform.com/d/-rfNCFznz/n3-api-management?orgId=110
 - **Application Gateway**  
-https://bigeye.sbp.eyclienthub.com/d/sB2bebk7k/n3-applicationgateway?orgId=110
+https://some_monitoring_platform.com/d/sB2bebk7k/n3-applicationgateway?orgId=110
 - **Event Hub**  
-https://bigeye.sbp.eyclienthub.com/d/hbWPWck7k/n3-eventhub?orgId=110
+https://some_monitoring_platform.com/d/hbWPWck7k/n3-eventhub?orgId=110
 - **Key Vault**  
-https://bigeye.sbp.eyclienthub.com/d/DfiSU1QMk/n3-key-vaults?orgId=110
+https://some_monitoring_platform.com/d/DfiSU1QMk/n3-key-vaults?orgId=110
 - **SQL Database**  
-https://bigeye.sbp.eyclienthub.com/d/NJc7CFz7z/n3-sql-database?orgId=110
+https://some_monitoring_platform.com/d/NJc7CFz7z/n3-sql-database?orgId=110
 - **Storage Account**  
-https://bigeye.sbp.eyclienthub.com/d/M8LSzMknk/n3-storageaccount?orgId=110
+https://some_monitoring_platform.com/d/M8LSzMknk/n3-storageaccount?orgId=110
 - **Traffic Manager**  
-https://bigeye.sbp.eyclienthub.com/d/oc4-61QMk/n3-traffic-manager?orgId=110
+https://some_monitoring_platform.com/d/oc4-61QMk/n3-traffic-manager?orgId=110
 '
 
 
